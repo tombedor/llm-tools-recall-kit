51e36961ab80ac912c4a2217db31aab10287986b
Tom Bedor <tombedor@gmail.com>
2025-05-27 19:04:35 -0700

    init commit

--- llm_tools_recall_kit.py
+++ llm_tools_recall_kit.py
@@ -1,13 +1,17 @@
 import llm
 
 
-def create_memory(input: str) -> str:
-    """
-    Description of tool goes here.
-    """
-    return f"hello {input}"
+class RecallKit(llm.Toolbox):
+    def create_memory(self, input: str) -> str:
+        """
+        Description of tool goes here.
+        """
+        return f"hello {input}"
+
+
+
 
 
 @llm.hookimpl
 def register_tools(register):
-    register(create_memory)
+    register(RecallKit)
